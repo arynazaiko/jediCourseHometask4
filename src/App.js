5f937645925789f26c5f904b19fb8bcdbfc2b6ff
Aryna Zaiko <arishazaiko@gmail.com>
2020-06-15 11:00:18 +0300

    Add navbar & routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-secondary mb-3">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">
+                  People
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/planets">
+                  Planets
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/starships">
+                  Starships
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div>
+          <Switch>
+            <Route exact path="/" />
+            <Route path="/planets" />
+            <Route path="/starships" />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
